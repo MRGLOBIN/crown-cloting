e3396a72678ee104be0b0fccd1983ee60aadf930
Muhammad Asim <asimqwe2@gmail.com>
2024-01-29 16:02:10 +0500

    added rank div and also particles-bg package

--- smartBrain/front-end/src/App.js
+++ smartBrain/front-end/src/App.js
@@ -1,14 +1,19 @@
-import Navigation from './componets/navigation/navigation'
 import Logo from './componets/Logo/logo'
+import Rank from './componets/rank/rank'
+import Navigation from './componets/navigation/navigation'
 import ImageLinkForm from './componets/imageLineForm/imageLinkForm'
+
+import ParticlesBg from 'particles-bg'
 
 import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg color="#F8F8FF" num={180} type="cobweb" bg={true} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
---
+++ smartBrain/front-end/src/componets/rank/rank.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Rank = () => {
+  return (
+    <div>
+      <div className="white f3">{'Goblin your current is...'}</div>
+      <div className="f1 white">{'#5'}</div>
+    </div>
+  )
+}
+
+export default Rank
