595a6ca71fd8ff6353395a063dfe33d8b128301f
Muhammad Asim <asimqwe2@gmail.com>
2024-01-29 09:19:56 +0500

    logo css added

--- smartBrain/front-end/src/componets/Logo/logo.js
+++ smartBrain/front-end/src/componets/Logo/logo.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './logo.css'
 import Tilt from 'react-parallax-tilt'
 
 const Logo = () => {
   return (
     <div className="ma4 mt0">
       <Tilt
-        className="br2 shadow-2"
+        className=" Tilt br2 shadow-2"
         style={{ height: '150px', width: '150px' }}
       >
         <div style={{ height: '300px' }}>👽</div>
