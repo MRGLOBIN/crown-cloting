009b795e5df7f667d9a71404157a3e9cb6d487ce
Muhammad Asim <asimqwe2@gmail.com>
2024-02-06 17:55:49 +0500

    modify: App component modified to class component

--- roboFriends/src/App.jsx
+++ roboFriends/src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 import CardList from './components/cardList/cardList'
 
 import 'tachyons'
 import './App.css'
 
-function App() {
-  return <CardList />
-}
+import { robots } from './constants/robots'
 
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots,
+      searchField: '',
+    }
+  }
+
+  render() {
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox />
+        <CardList robots={this.state.robots} />
+      </div>
+    )
+  }
+}
 export default App
