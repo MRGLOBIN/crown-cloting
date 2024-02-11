23d38e1e76a3267d9d6265c3e005c2bdb9ac1856
Muhammad Asim <asimqwe2@gmail.com>
2024-02-11 16:41:35 +0500

    add: mobile response to hero

--- portfolio/src/components/canvas/Computers.jsx
+++ portfolio/src/components/canvas/Computers.jsx
@@ -1,36 +1,61 @@
-import React, { Suspense, useEffect, useState } from 'react'
+import React, { Suspense, useEffect, useRef, useState } from 'react'
 
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
 import CanvasLoader from '../Loader'
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+      <hemisphereLight intensity={1.5} groundColor="black" />
+      <pointLight intensity={0.5} />
+      <ambientLight intensity={0.6} />
 
-      <pointLight intensity={1} />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
-        intensity={1}
+        intensity={50}
         castShadow
         shadow-mapSize={1024}
       />
+
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
-        rotation={[-0.1, -0.2, -0.1]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3.25, -1.5] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   )
 }
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobie] = useState(false)
+
+  useEffect(() => {
+    // adding event listener for screen size
+    const mediaQuery = window.matchMedia('(max-width: 500px)')
+
+    // setting to according to intital state of screen
+    setIsMobie(mediaQuery.matches)
+
+    // function to change screen mobile state
+    const handleMediaQueryChange = event => {
+      setIsMobie(event.matches)
+    }
+
+    // listening to change and set screen accordingly
+    mediaQuery.addEventListener('change', handleMediaQueryChange)
+
+    // removing the event listeniner to avoid memory leaks
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+  })
+
   return (
     <Canvas
       frameloop="demand"
@@ -38,13 +63,13 @@ const ComputersCanvas = () => {
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-      <Suspense fallback={<CanvasLoader />}>
+      <Suspense fallback={''}>
         <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
