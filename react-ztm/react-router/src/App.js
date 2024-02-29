f1519a2b13e750064ca2914e07ef7e575fc095e0
Muhammad Asim <asimqwe2@gmail.com>
2024-02-29 23:29:37 +0500

    initate: monsterRolodex vite;

---
+++ react-ztm/react-router/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import StarSky from 'react-star-sky'
+import 'react-star-sky/dist/index.css'
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <StarSky />
+    </div>
+  )
+}
+
+export default App
