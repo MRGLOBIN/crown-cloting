2eebabc77e970e234252b99095d341db11cd372a
Muhammad Asim <asimqwe2@gmail.com>
2024-03-19 09:28:27 +0500

    react basics: 24

--- react-ztm/monsterRolodex/src/App.jsx
+++ react-ztm/monsterRolodex/src/App.jsx
@@ -10,15 +10,19 @@ class App extends Component {
     this.state = {
       monsters: [
         {
+          id: 1,
           name: 'linda',
         },
         {
+          id: 2,
           name: 'jackey',
         },
         {
+          id: 3,
           name: 'Frank',
         },
         {
+          id: 4,
           name: 'Goblin',
         },
       ],
@@ -29,7 +33,7 @@ class App extends Component {
     return (
       <>
         {this.state.monsters.map(monster => {
-          return <h1>{monster.name}</h1>
+          return <h1 key={monster.id}>{monster.name}</h1>
         })}
       </>
     )
