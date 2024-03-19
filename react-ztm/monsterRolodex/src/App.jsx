2f49d47bbc538c3445f0645e686e72491d6a2c6a
Muhammad Asim <asimqwe2@gmail.com>
2024-03-19 09:17:09 +0500

    react basics: 22

--- react-ztm/monsterRolodex/src/App.jsx
+++ react-ztm/monsterRolodex/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
+import { Component } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      monsters: [
+        {
+          name: 'linda',
+        },
+        {
+          name: 'jackey',
+        },
+        {
+          name: 'Frank',
+        },
+        {
+          name: 'Goblin',
+        },
+      ],
+    }
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  render() {
+    return (
+      <>
+        {this.state.monsters.map(monster => {
+          return <h1>{monster.name}</h1>
+        })}
+      </>
+    )
+  }
 }
 
 export default App
