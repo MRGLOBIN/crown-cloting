58be783dd5ece426c73c5171af0cd749e864c794
Muhammad Asim <111900132+MRGLOBIN@users.noreply.github.com>
2024-07-04 13:34:32 +0500

    Delete angularBasic directory

--- angularBasic/codelabs-homes-app-start/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser'
-import { AppComponent } from './app/app.component'
-import { provideRouter } from '@angular/router'
-
-import routeConfig from './app/routes'
-
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routeConfig)],
-}).catch(err => console.error(err))
